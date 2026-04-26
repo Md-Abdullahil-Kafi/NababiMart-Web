3649fd649631ec530a054634e1a5231390a5ca9c
Md. Abdullahil Kafi <ahkafi2003@gmail.com>
2026-04-26 21:57:35 +0600

    some changes of second version

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 700, // optional (warning reduce)
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase'],
-          motion: ['framer-motion'],
-          swiper: ['swiper'],
-          icons: ['react-icons'],
-        },
-      },
-    },
+    chunkSizeWarningLimit: 700,
   },
 })
